62fef24d8f3ec48f9d448039c535f7dfe7c3473f
Mondris <justmondris@gmail.com>
2021-01-25 08:48:30 +0100

    add @Repository annotation to userRepository

--- dris.auth-service/src/main/java/com/etranzact/dris/authservice/dris/authservice/Repository/UserRepository.java
+++ dris.auth-service/src/main/java/com/etranzact/dris/authservice/dris/authservice/Repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.etranzact.dris.authservice.dris.authservice.Repository;
 
 import com.etranzact.dris.authservice.dris.authservice.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User getByEmail(String email);
 }
