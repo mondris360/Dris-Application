99a66a970db04e8c2221689b95590c0a24f7385f
Mondris <justmondris@gmail.com>
2021-01-25 08:25:51 +0100

    Add service registry client

--- dris.auth-service/src/main/java/com/etranzact/dris/authservice/dris/authservice/Application.java
+++ dris.auth-service/src/main/java/com/etranzact/dris/authservice/dris/authservice/Application.java
@@ -3,11 +3,13 @@ package com.etranzact.dris.authservice.dris.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableCaching
+@EnableEurekaClient
 public class Application {
 
 	public static void main(String[] args) {
