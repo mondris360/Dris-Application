26dec22c31dd039813d5f40d360e80926c00d392
Mondris <justmondris@gmail.com>
2021-02-16 00:37:43 +0100

    Add rabbitmq dependency

---
+++ dris.auth-service/src/main/java/com/etranzact/dris/authservice/dris/authservice/Dto/SendEmailReqDto.java
@@ -0,0 +1,4 @@
+package com.etranzact.dris.authservice.dris.authservice.Dto;
+
+public class SendEmailReqDto {
+}
---
+++ dris.auth-service/src/main/java/com/etranzact/dris/authservice/dris/authservice/Util/RabbitMQ/RabbitMQConfig.java
@@ -0,0 +1,12 @@
+package com.etranzact.dris.authservice.dris.authservice.Util.RabbitMQ;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public AmqpTemplate amqpTemplate(){
+
+    }
+}
