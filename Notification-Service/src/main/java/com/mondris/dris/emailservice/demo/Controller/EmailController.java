173855e3be74b40187be01a5b68def5397f070cc
Mondris <justmondris@gmail.com>
2021-02-16 00:33:49 +0100

    Add RabbitListener

--- Notification-Service/src/main/java/com/mondris/dris/emailservice/demo/Controller/EmailController.java
+++ Notification-Service/src/main/java/com/mondris/dris/emailservice/demo/Controller/EmailController.java
@@ -4,6 +4,7 @@ package com.mondris.dris.emailservice.demo.Controller;
 import com.mondris.dris.emailservice.demo.Dto.SendMailRequest;
 import com.mondris.dris.emailservice.demo.Dto.SendMailResponse;
 import com.mondris.dris.emailservice.demo.Service.EmailService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,9 +18,10 @@ public class EmailController {
 
     @Resource
     private EmailService emailService;
-
+    @RabbitListener(queues = "EmailQueue")
     @PostMapping("/sendEmail")
     ResponseEntity<SendMailResponse> sendEmail(@Valid @RequestBody  SendMailRequest request) throws Exception {
+        System.out.println("received email request from queue+" + request);
         System.out.println("request" +  request);
         return  emailService.sendEmail(request);
     }
