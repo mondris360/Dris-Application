3b7a582c91474fa8652e6e07bf50cff62ef90dbd
Mondris <justmondris@gmail.com>
2021-03-03 01:24:35 +0100

    Add more fields to SignUpRequestDto

--- dris.auth-service/src/main/java/com/etranzact/dris/authservice/dris/authservice/Dto/SignUpRequestDto.java
+++ dris.auth-service/src/main/java/com/etranzact/dris/authservice/dris/authservice/Dto/SignUpRequestDto.java
@@ -4,7 +4,10 @@ import com.etranzact.dris.authservice.dris.authservice.Dto.BaseDto.BaseDto;
 import com.etranzact.dris.authservice.dris.authservice.Model.Authority;
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 import java.util.Set;
 
@@ -15,6 +18,39 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SignUpRequestDto extends BaseDto{
+
+    @Column(name="first_name")
+    @NotBlank(message = "firstName is mandatory")
+    private String firstName;
+
+    @Column(name="last_name")
+    @NotBlank(message = "lastName is mandatory")
+    private String lastName;
+
+    @NotBlank(message = "email is mandatory")
+    @Email
+    private String email;
+
+    @NotBlank( message = "Office position is compulsory")
+    private String officePosition;
+
+    @NotBlank(message = "mobile Number is mandatory")
+    private String mobileNumber;
+
+    private String officeNumber;
+
+    @NotBlank(message = "streetAddress is mandatory")
+    private String streetAddress;
+
+    @NotBlank(message = "city is mandatory")
+    private String city;
+
+    @NotBlank(message = "state is mandatory")
+    private String state;
+
+    @NotBlank(message = "country is mandatory")
+    private String country;
+
     private Set< @Valid Authority> authorities;
 
 }
