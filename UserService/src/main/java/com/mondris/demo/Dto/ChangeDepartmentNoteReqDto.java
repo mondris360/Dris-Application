29698c357175b0e830923aa6ff5380be31c1f661
Mondris <justmondris@gmail.com>
2021-03-30 17:26:06 +0100

    Create ChangeDepartmentNoteReqDto

---
+++ UserService/src/main/java/com/mondris/demo/Dto/ChangeDepartmentNoteReqDto.java
@@ -0,0 +1,20 @@
+package com.mondris.demo.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@AllArgsConstructor
+@Setter
+@Getter
+public class ChangeDepartmentNoteReqDto {
+    @NotBlank(message = "departmentId is mandatory")
+    private long departmentId;
+
+    @NotBlank(message = "newNote is mandatory")
+    @Size(max =  2000, message = "newNote must  be lesser than or equal to 2000 characters")
+    private  String newNote;
+}
