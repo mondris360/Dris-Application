82e7ae9f9efeaaa2bec251c0ea016b9240bcf5f2
Mondris <justmondris@gmail.com>
2021-04-07 20:43:48 +0100

    Create office position repository

---
+++ UserService/src/main/java/com/mondris/demo/Repository/OfficePositionRepository.java
@@ -0,0 +1,8 @@
+package com.mondris.demo.Repository;
+
+import com.mondris.demo.Model.OfficePosition;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OfficePositionRepository extends JpaRepository<OfficePosition, Long> {
+
+}
